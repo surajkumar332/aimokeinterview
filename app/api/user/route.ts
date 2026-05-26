1111305bdd61d7f6c9de42364ce2b976c9d6d947
Suraj Kumar332 <surajk71621@gmail.com>
2026-05-26 14:49:39 +0530

    error solved2

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/Mongodb";
+import { connectDB } from "@/lib/mongodb";
 import { User } from "@/models/User";
 
 export async function POST(req: Request) {
